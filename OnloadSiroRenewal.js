6dcd127c95df03306eace34598688643a7c01b25
Hrithik <124701655+Hrithik-0210@users.noreply.github.com>
2024-01-03 17:19:41 +0530

    3 jan function onload

---
+++ OnloadSiroRenewal.js
@@ -0,0 +1,225 @@
+// 1st section START:
+$("#124179_additionalDoc").closest(".trow").hide(); //1a
+$("#124180_additionalDoc").closest(".trow").hide(); //1b
+$("#124181_additionalDoc").closest(".trow").hide(); //1c
+$("#124182_additionalDoc").closest(".trow").hide(); //1d
+
+$("#124204_0").val("/");
+$("#124204_0").prop("readonly", true);
+$("#133072_0").val("-TU-V");
+$("#133072_0").prop("readonly", true);
+//1st section END :
+
+//Declaration START :
+$("#123710_openCamra").hide();
+//Declaration END :
+
+// 2nd section START:
+$("#123747").closest(".trow").hide(); //Pan1
+$("#123748").closest(".trow").hide(); //Pan2
+$("#123721_additionalDoc").closest(".trow").hide(); //Pan3
+$("#123719_additionalDoc").closest(".trow").hide(); // 2.2>Colleges>123719.additionalDoc
+$("#123720_additionalDoc").closest(".trow").hide(); //2.2>Colleges>123720.additionalDoc
+$("#123722").closest(".trow").hide(); // 2.2>fromOption1 to option4>Tan1
+$("#123723_additionalDoc").closest(".trow").hide(); //2.2>fromOption1 to option4>Tan1
+
+//     2nd Section end
+
+//2.4(a) start :
+$("#133098").closest("[data-type=section]").hide();
+//2.4(a) end
+
+//2.4(b) start :
+$("#133108").closest("[data-type=section]").hide();
+//2.4(b) end :
+
+//    5th Section START :
+$("#123695_additionalDoc").closest(".trow").hide(); //5.2>123695 gramSection5
+//   5th section END
+
+//6TH Section start :
+$("#123778").closest(".trow").hide(); // 6.3>123778"#"+gramSection6S31
+$("123779_additionalDoc").closest(".trow").hide(); //6.3>123779_additionalDoc"#"+gramSection6S32
+$("123786_additionalDoc").closest(".trow").hide(); //6.4>123786_additionalDoc"#"+gramSection6SU
+
+//   7th Section START :
+$("123800_additionalDoc").closest(".trow").hide(); //upload on No //
+//   7th Section END :
+
+//7.1 section start :
+$("#123831_0").closest("[data-type=section]").hide();
+//7.1 section end
+
+//7.2 section start :
+$("#123846_0").closest("[data-type=section]").hide();
+//7.2 section end
+
+//7.3 section start :
+$("#123858_0").closest("[data-type=section]").hide();
+//7.3 section end
+
+//   9th Section START :
+$("#123873").closest(".trow").hide(); //gramSection9
+//   9th Section END :
+
+// var num1RDy1Inc
+$("#123831_0").val("For R&D Activities");
+$("#123831_0").css({ "font-weight": "bold" });
+$("#123831_0").prop("readonly", true);
+
+//var num2RDy1Exp
+$("#123837_0").val("For R&D Activities");
+$("#123837_0").css({ "font-weight": "bold" });
+$("#123837_0").prop("readonly", true);
+
+//var num3RDy2Inc
+$("#123846_0").val("For R&D Activities");
+$("#123846_0").css({ "font-weight": "bold" });
+$("#123846_0").prop("readonly", true);
+
+// var num4RDy2Exp
+$("#123808_0").val("For R&D Activities");
+$("#123808_0").css({ "font-weight": "bold" });
+$("#123808_0").prop("readonly", true);
+
+// var num5RDy3Inc
+$("#123858_0").val("For R&D Activities");
+$("#123858_0").css({ "font-weight": "bold" });
+$("#123858_0").prop("readonly", true);
+
+// var num6RDy3Exp
+$("#123867_0").val("For R&D Activities");
+$("#123867_0").css({ "font-weight": "bold" });
+$("#123867_0").prop("readonly", true);
+
+// var num1NRDy1Inc
+$("#123831_1").val("For Non R&D Activities");
+$("#123831_1").css({ "font-weight": "bold" });
+$("#123831_1").prop("readonly", true);
+
+// var num2NRDy1Exp
+$("#123837_1").val("For Non R&D Activities");
+$("#123837_1").css({ "font-weight": "bold" });
+$("#123837_1").prop("readonly", true);
+
+// var num3NRDy2Inc
+$("#123846_1").val("For Non R&D Activities");
+$("#123846_1").css({ "font-weight": "bold" });
+$("#123846_1").prop("readonly", true);
+
+// var num4NRDy2Exp
+$("#123808_1").val("For Non R&D Activities");
+$("#123808_1").css({ "font-weight": "bold" });
+$("#123808_1").prop("readonly", true);
+
+// var num5NRDy3Inc
+$("#123858_1").val("For Non R&D Activities");
+$("#123858_1").css({ "font-weight": "bold" });
+$("#123858_1").prop("readonly", true);
+
+// var num6NRDy3Exp
+$("#123867_1").val("For Non R&D Activities");
+$("#123867_1").css({ "font-weight": "bold" });
+$("#123867_1").prop("readonly", true);
+
+// var y1incT
+$("#123831_2").val("Total Income");
+$("#123831_2").css({ "font-weight": "bold" });
+$("#123831_2").prop("readonly", true);
+
+// var y1expT
+$("#123837_2").val("Total Expenditure");
+$("#123837_2").css({ "font-weight": "bold" });
+$("#123837_2").prop("readonly", true);
+
+// var y2incT
+$("#123846_2").val("Total Income");
+$("#123846_2").css({ "font-weight": "bold" });
+$("#123846_2").prop("readonly", true);
+
+// var y2expT
+$("#123808_2").val("Total Expenditure");
+$("#123808_2").css({ "font-weight": "bold" });
+$("#123808_2").prop("readonly", true);
+
+// var y3incT
+$("#123858_2").val("Total Income");
+$("#123858_2").css({ "font-weight": "bold" });
+$("#123858_2").prop("readonly", true);
+
+// var y3expT
+$("#123867_2").val("Total Expenditure");
+$("#123867_2").css({ "font-weight": "bold" });
+$("#123867_2").prop("readonly", true);
+
+// var Sec13Q1A
+$("#123926_0").val("PhD engaged in R&D");
+$("#123926_0").css({ "font-weight": "bold" });
+$("#123926_0").prop("readonly", true);
+
+// var Sec13Q2A
+$("#123926_1").val("ME/M.Tech engaged in R&D");
+$("#123926_1").css({ "font-weight": "bold" });
+$("#123926_1").prop("readonly", true);
+
+// var Sec13Q3A
+$("#123926_2").val("Other PG(s) engaged in R&D");
+$("#123926_2").css({ "font-weight": "bold" });
+$("#123926_2").prop("readonly", true);
+
+// var Sec13Q4A
+$("#123926_3").val("Other Staffs engaged in R&D");
+$("#123926_3").css({ "font-weight": "bold" });
+$("#123926_3").prop("readonly", true);
+
+// var Sec13Q5A
+$("#123926_4").val("Total Manpower engaged in R&D");
+$("#123926_4").css({ "font-weight": "bold" });
+$("#123926_4").prop("readonly", true);
+
+// var Sec13Q6A
+$("#123926_5").val("Manpower engaged in Non R&D activities");
+$("#123926_5").css({ "font-weight": "bold" });
+$("#123926_5").prop("readonly", true);
+
+// var Sec13Q7A
+$("#123926_6").val("Total Manpower with the Organization");
+$("#123926_6").css({ "font-weight": "bold" });
+$("#123926_6").prop("readonly", true);
+
+$("#123719_additionalDoc").closest(".trow").hide();
+$("#123720_additionalDoc").closest(".trow").hide();
+
+// var sec10TLA1
+$("#123894_0").val("Total Land & Building area for R&D");
+$("#123894_0").css({ "font-weight": "bold" });
+$("#123894_0").prop("readonly", true);
+
+// var sec10TLA2
+$("#123894_1").val("Total Land & Building area with Organization");
+$("#123894_1").css({ "font-weight": "bold" });
+$("#123894_1").prop("readonly", true);
+
+// var R17t1
+$("#124900_0").val("No. of Research Papers /Articles etc.");
+$("#124900_0").css({ "font-weight": "bold" });
+$("#124900_0").prop("readonly", true);
+
+// var R17t2
+$("#124021_0").val("No. of Books /Book-chapters etc.");
+$("#124021_0").css({ "font-weight": "bold" });
+$("#124021_0").prop("readonly", true);
+
+// var R17t3
+$("#124908_0").val("Duty Exemption (claimed in Lakhs)");
+$("#124908_0").css({ "font-weight": "bold" });
+$("#124908_0").prop("readonly", true);
+
+// var R19t1
+$("#124049_0").val(
+  "No. of Seminars, Conferences, Workshops, Training courses conducted during last 3 years"
+);
+$("#124049_0").css({ "font-weight": "bold" });
+$("#124049_0").prop("readonly", true);
+
+$("#124680").closest(".trow").hide(); //gramSection20
